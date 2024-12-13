9da5d462df74d0ff1f34dbcabdd7e57bb455f711
Tahsin Özgür KOÇ <tahsinkoc6@gmail.com>
2024-12-13 18:13:35 +0300

    fxacaxca

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -1,17 +1,10 @@
 export default function Head() {
   return (
     <>
-<<<<<<< HEAD
       <title>Digitalhat</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Built with Next.js and TypeScript" />
       <link rel="icon" href="/favicon.ico" />
-=======
-      <title> Digitalhat </title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Digitalhat Software Solutions" />
-      <link rel="icon" href="/images/favicon.ico" />
->>>>>>> ff84c8fbbf2b01cf56039a5d43ed70420ec76e8a
     </>
   );
 }
